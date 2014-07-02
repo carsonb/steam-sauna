c0c58d94748c6ad478b75aec3ff915423808f227
Carson Brown <carson@carsonbrown.ca>
2014-07-02 19:00:12 -0400

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SteamSauna::Application.config.secret_key_base = '0a63ac1e949bb75077c373d0c5e28dde9410570b3f2f7f433ce75c9ae152a9bc6e7720f7212a8909367793e6bf3b99fb007b7e1d12809aee8c57bedd9c54f541'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SteamSauna::Application.config.session_store :cookie_store, key: '_steam_sauna_session'
