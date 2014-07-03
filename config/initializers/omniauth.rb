cd06a27f8c433340d5d03a3d69bf407c36a65f34
Carson Brown <carson@carsonbrown.ca>
2014-07-02 21:01:03 -0400

    Removes old key that was refactored

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, ENV["STEAM_WEB_API_KEY"]
+  provider :steam, ENV["STEAM_API_KEY"]
 end
