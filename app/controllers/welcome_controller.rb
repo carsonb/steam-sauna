da8f3be06c5102c0d0ce3af785f9ccaebfa660cf
Carson Brown <carson@carsonbrown.ca>
2014-07-06 19:57:31 -0400

    Refactors steam-api wrapping into separate service object

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,37 +1,19 @@
 class WelcomeController < ApplicationController
   # auth callback POST comes from Steam so we can't attach CSRF token
   skip_before_filter :verify_authenticity_token, :only => :auth_callback
-  before_action :ensure_logged_in
 
   def index
-    @friends = retrieve_friends
-    @games = retrieve_games(user_id)
+    @friends = steam_service.retrieve_friends
+    @games = steam_service.retrieve_games(user_id)
   end
 
   def search
-    finder = GameFinder.new
-    finder.add_players_games(retrieve_games(user_id))
-    params[:friends].each do |friend_id|
-      finder.add_players_games(retrieve_games(friend_id))
-    end
-    @games_to_play = finder.playable_games
+    @games_to_play = steam_service.find_matching_games(params[:friends])
   end
 
-  def retrieve_friends
-    begin
-      friends = Steam::User.friends(user_id)
-    rescue Steam::JSONError
-      return []
-    end
-    Steam::User.summaries(friends.map{|f| f['steamid']}).sort { |a,b| a['personaname'].downcase <=> b['personaname'].downcase }
-  end
+  private
 
-  def retrieve_games(uid)
-    begin
-      games = Steam::Player.owned_games(uid, params: {include_appinfo: 1})
-    rescue Steam::JSONError
-      games = []
-    end
-    games['games'].map { |game| Game.new(game) }
+  def steam_service
+    @steam_service ||= SteamService.new(user_id) if user_id
   end
 end
