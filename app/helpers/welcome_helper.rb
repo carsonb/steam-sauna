b3588fb8aa32d2e3900d0f8cae0c2ee4a6477565
Carson Brown <carson@carsonbrown.ca>
2014-08-05 20:48:06 -0400

    Refactor URLs into the model
    No one else needs them

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,17 +1,9 @@
 module WelcomeHelper
   def game_icon(game)    
-    if game.img_icon_url.present?
-      image_tag("http://media.steampowered.com/steamcommunity/public/images/apps/#{game.appid}/#{game.img_icon_url}.jpg", alt: game.name)
-    else
-      image_tag("http://placehold.it/32x32.png/000000/ffffff&text=#{game.name.first}", alt: game.name)
-    end
+    image_tag(game.icon_url, alt: game.name)
   end
 
   def game_logo(game)
-    if game.img_logo_url.present?
-      image_tag("http://media.steampowered.com/steamcommunity/public/images/apps/#{game.appid}/#{game.img_logo_url}.jpg", alt: game.name)
-    else
-      image_tag("http://placehold.it/184x69.png/000000/ffffff&text=#{URI.escape(game.name.truncate(12, omission: '…'))}", alt: game.name)
-    end
+    image_tag(game.logo_url, alt: game.name)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,20 @@ class Game
     @img_logo_url = hash['img_logo_url']
     @img_icon_url = hash['img_icon_url']
   end
+
+  def icon_url
+    if @img_icon_url.present?
+      "http://media.steampowered.com/steamcommunity/public/images/apps/#{@appid}/#{@img_icon_url}.jpg"
+    else
+      "http://placehold.it/32x32.png/000000/ffffff&text=#{@name.first}"
+    end
+  end
+
+  def logo_url
+    if @img_logo_url.present?
+      "http://media.steampowered.com/steamcommunity/public/images/apps/#{@appid}/#{@img_logo_url}.jpg"
+    else
+      "http://placehold.it/184x69.png/000000/ffffff&text=#{URI.escape(@name.truncate(12, omission: '…'))}"
+    end
+  end
 end
